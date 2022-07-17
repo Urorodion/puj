6ead6eecb452a48761a35bbe60674d3fde31fc6c
Urorodion <94755623+Urorodion@users.noreply.github.com>
2022-07-17 21:28:44 +0400

    Add files via upload

--- js.js
+++ js.js
@@ -4,17 +4,6 @@ const logo = document.querySelector('.logo__video')
 const app = document.querySelector('.app-page')
 const load = document.querySelector('.load-page')
 
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 function firstCounter(ms) {
     let counterFirst = 0;
     let interval = setInterval(() => {
