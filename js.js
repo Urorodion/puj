a686f5f04bdb7fa1e2658f0d55964a9fd4c78c4c
Urorodion <94755623+Urorodion@users.noreply.github.com>
2022-07-17 21:55:45 +0400

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,18 @@
 const fontsWeight = ['300', '400', '500', '900'];
-const first = document.querySelector('.timer__number')
-const logo = document.querySelector('.logo__video')
-const app = document.querySelector('.app-page')
-const load = document.querySelector('.load-page')
+const first = document.querySelector('.timer__number');
+const logo = document.querySelector('.logo__video');
+const app = document.querySelector('.app-page');
+const load = document.querySelector('.load-page');
+
+
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 
 function firstCounter(ms) {
     let counterFirst = 0;
@@ -20,9 +30,6 @@ function randomweight() {
     const index = Math.floor(Math.random() * fontsWeight.length);
     return fontsWeight[index];
 }
-
-
-
 
 
 setTimeout(() => {
