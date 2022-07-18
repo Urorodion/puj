ea27921d7f396d2e4f1a6783e551db02c78b82f1
Urorodion <94755623+Urorodion@users.noreply.github.com>
2022-07-18 23:45:19 +0400

    Add files via upload

--- js.js
+++ js.js
@@ -3,6 +3,9 @@ const first = document.querySelector('.timer__number');
 const logo = document.querySelector('.logo__video');
 const app = document.querySelector('.app-page');
 const load = document.querySelector('.load-page');
+const logo_white__video = document.querySelector('.logo-white__video');
+const logo_white_img = document.querySelector('.logo-white__img');
+const logo_white = document.querySelector('.logo-white');
 
 
 
@@ -34,8 +37,16 @@ function randomweight() {
 
 setTimeout(() => {
     first.style.display = 'none';
-    logo.style.filter = 'none';
-    load.style.display = 'none';
-    app.style.display = 'block';
+    load.style.opacity = '0';
+    app.style.display = 'grid';
 }, 7000);
 
+
+setTimeout(() => {
+    logo_white__video.style.display = 'block';
+    logo.style.filter = 'none';
+}, 7300);
+setTimeout(() => {
+    logo_white_img.style.display = 'block';
+    logo_white__video.style.display = 'none';
+}, 8900);
